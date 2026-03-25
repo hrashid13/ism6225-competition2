62b73c12bbcb8d06180918c3fbb506e9d5cec98e
Savannah Day <day18@usf.edu>
2026-03-25 11:47:23 -0400

    Updated Book class

--- Competition_2_ISM_6225/Book.cs
+++ Competition_2_ISM_6225/Book.cs
@@ -2,5 +2,39 @@
 {
     public class Book
     {
+     public string Title { get; set; }
+    public string Author { get; set; }
+    public string ISBN { get; set; }
+    public int AvailableCopies { get; set; }
+
+    public Book(string title, string author, string isbn, int availableCopies)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+        AvailableCopies = availableCopies;
     }
+
+    public bool BorrowBook()
+    {
+        if (AvailableCopies > 0)
+        {
+            AvailableCopies--;
+            return true;
+        }
+        return false;
+    }
+
+    public void ReturnBook()
+    {
+        AvailableCopies++;
+    }
+
+    public override string ToString()
+    {
+        return $"Title: {Title}, Author: {Author}, Available Copies: {AvailableCopies}";
+    }
+
+}
+
 }
