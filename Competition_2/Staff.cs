e759422db7ef0d3782cb52cdf73aed83ab8b3456
Gustavo Storck <gustavow@usf.edu>
2026-03-25 13:01:31 -0400

    Refactor Staff class to inherit from Person

--- Competition_2/Staff.cs
+++ Competition_2/Staff.cs
@@ -4,16 +4,16 @@ using System.Text;
 
 namespace Competition_2
 {
-    internal class Staff
-    {
-        public string Position { get; set; }
-        public string Department { get; set; }
+    public class Staff : Person
+{
+    public string Position { get; set; }
+    public string Department { get; set; }
 
-        public Staff(string name, string email, string id, string position, string department)
-            : base(name, email, id)
-        {
-            Position = position;
-            Department = department;
-        }
+    public Staff(string name, string email, string id, string position, string department)
+        : base(name, email, id)
+    {
+        Position = position;
+        Department = department;
     }
+}
 }
