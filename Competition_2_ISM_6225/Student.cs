e79c4eca318fc4b0ebaa8b0b1048a8b66187a868
gustavows <gustavow@usf.edu>
2026-03-25 11:42:59 -0400

    added staff, person and student classes

--- Competition_2_ISM_6225/Student.cs
+++ Competition_2_ISM_6225/Student.cs
@@ -2,5 +2,14 @@
 {
     public class Student
     {
+        public string Major { get; set; }
+        public int GraduationYear { get; set; }
+
+        public Student(string name, string email, string id, string major, int gradYear)
+            : base(name, email, id)
+        {
+            Major = major;
+            GraduationYear = gradYear;
+        }
     }
 }
